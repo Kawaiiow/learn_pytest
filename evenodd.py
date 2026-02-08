82938d6d34f83ce84d18c65c699f2d123975a4a7
Kawaiiow <poohsn1357@gmail.com>
2026-02-08 07:08:53 +0700

    test correct code

--- evenodd.py
+++ evenodd.py
@@ -2,7 +2,7 @@
 
 def is_even_odd(n : int) -> str:
 	"""Function to print is the number is even or odd"""
-	return "Even" if not (n % 2) else "Odds"
+	return "Even" if not (n % 2) else "Odd"
 
 if __name__ == "__main__":
 	num = int(input("Enter a Number: "))
